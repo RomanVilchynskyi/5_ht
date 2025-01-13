aa17939cd565a75d1c7a066d056c225e95b42052
Roman <VXlch_vg85@student.itstep.org>
2025-01-13 17:35:52 +0200

    08_ht

---
+++ 08_ht/Fraction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_ht
+{
+    struct Vector3d
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Vector3d(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector3d MultByNum(double num)
+        {
+            return new Vector3d(X * num, Y * num, Z * num);
+        }
+
+        public Vector3d Add(Vector3d other)
+        {
+            return new Vector3d(X + other.X, Y + other.Y, Z + other.Z);
+        }
+
+        public Vector3d Sub(Vector3d other)
+        {
+            return new Vector3d(X - other.X, Y - other.Y, Z - other.Z);
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z})";
+        }
+    }
+}
---
+++ 08_ht/Program.cs
@@ -0,0 +1,19 @@
+﻿using _08_ht;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Vector3d vector1 = new Vector3d(2, 3, 4);
+        Vector3d vector2 = new Vector3d(1, 1, 1);
+
+        Vector3d mv = vector1.MultByNum(2);
+        Console.WriteLine($"Multiplication: {mv}");
+
+        Vector3d av = vector1.Add(vector2);
+        Console.WriteLine($"Addition: {av}");
+
+        Vector3d sv = vector1.Sub(vector2);
+        Console.WriteLine($"Subtraction: {sv}");
+    }
+}
